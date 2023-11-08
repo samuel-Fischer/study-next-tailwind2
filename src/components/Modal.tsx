dbcabecb9e50225555d54900272d80faf717712e
Samuel Fischer <samuelfisklug@gmail.com>
2023-11-08 17:21:11 -0300

    Add proposal form to Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -75,7 +75,8 @@ export function Modal({ isVisible, onClose, clientId, nome, carroId,  date,  fot
       <div className="w-[600px] flex flex-col">
         <button className="text-white text-xl place-self-end" onClick={closeModal}>X</button>
         <div className="bg-white p-2 rounded">
-          <div className="col-span-3 flex flex-auto">
+          <h1 className="text-2xl font-bold text-center text-primary-red">Faça sua proposta</h1>
+          <div className="mt-2">
             <label htmlFor="lance" className="ano">
               <span className="block font-medium text-slate-700 text-lg">
                 Preço desejado:
@@ -91,25 +92,34 @@ export function Modal({ isVisible, onClose, clientId, nome, carroId,  date,  fot
                 "
                 {...register("lance", { required: true })}
               />
-              <input
-                id="texto"
-                type="text"
-                className="mt-1 w-[100] px-3 py-2 max-lg bg-slate-100 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-                focus:outline-none focus:border-red-500 focus:ring-1 focus:ring-red-500
-                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                invalid:border-pink-500 invalid:text-pink-600 valid:border-green-500 valid:text-green-600
-                focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-                "
+
+          <div className="flex flex-col mt-4">
+            <label
+              htmlFor="sobre"
+              className="block text-lg font-medium leading-6 text-gray-900"
+            >
+              Qual sua proposta adicional para este veiculo:
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="sobre"
+                rows={3}
+                className="bg-slate-100 rounded-md w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 focus:border-red-500 focus:ring-1 focus:ring-red-500"
+                defaultValue={""}
                 {...register("texto", { required: true })}
               />
+            </div>
+          </div>
+
+
+
       
               <button
-                className="bg-green-600 hover:bg-green-500 text-white font-bold ms-2 py-2 px-4 rounded"
+                className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mt-2"
                 onClick={handleSubmit(salvarProposta)}
               >
                 Enviar proposta
               </button>
-              <div className="pt-2"></div>
             </label>
           </div>
         </div>
